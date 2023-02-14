61abf6587eb816d5813a11b45f62d5b5cb28cff9
nocnoc_e <dhgustmd25@hotmail.com>
2023-02-14 09:10:57 -0500

    changed

--- app/models/user.js
+++ app/models/user.js
@@ -1,28 +1,58 @@
 const mongoose = require('mongoose')
+const { Schema, model } = mongoose
 
 const userSchema = new mongoose.Schema(
 	{
+		profilePicture: {
+		  type: String,
+		  data: Buffer
+		},
+		coverPicture: {
+			type: String,
+			data: Buffer
+		},
+		followers: [
+			{type: Schema.Types.ObjectId,
+			ref: 'User'} 
+		],
+		followings: [
+			{type: Schema.Types.ObjectId,
+			ref: 'User'} 
+		],
+		username: { 
+			type: String, 
+			required: true, 
+			unique: true 
+		},
+		password: { 
+			type: String, 
+			required: true 
+		},
 		email: {
-			type: String,
-			required: true,
-			unique: true,
+			type: String, 
+			required: true 
 		},
-		hashedPassword: {
-			type: String,
-			required: true,
+		password_confirmation: {
+			type: String
 		},
-		token: String,
-	},
-	{
-		timestamps: true,
-		toObject: {
-			// remove `hashedPassword` field when we call `.toObject`
-			transform: (_doc, user) => {
-				delete user.hashedPassword
-				return user
-			},
+		description: {
+			type: String
 		},
-	}
+		city: {
+			type: String
+		},
+		active: {
+			type: Boolean
+		}
+	
+	  }, {
+		  timestamps: true,
+		  toObject: { virtuals: true },
+		  toJSON: { virtuals: true }
+	  }
 )
 
-module.exports = mongoose.model('User', userSchema)
+// Converting to Model
+const User = model('User', userSchema)
+
+module.exports = User
