bf30938e6afcec940e66e6e5d9241cd03f422c7d
nocnoc_e <dhgustmd25@hotmail.com>
2023-02-19 19:25:32 -0500

    need to update and delete

--- app/routes/comment_routes.js
+++ app/routes/comment_routes.js
@@ -47,33 +47,37 @@ router.post(`/:userId`, (req,res, next)=>{
 //------------ UPDATE Comment ------------------
 // PATCH ROUTES -> /comments/:userId
 
-router.patch(`/:userId`, (req,res, next)=>{
-    const userId = req.params.userId
-    Comment.findOneAndUpdate({owner: userId}, {
-        usercomment: req.body.usercomment
-    })
-    .then(handle404)
-    .then(comment=> {
-        console.log(`UPDATED A NEW COMMENT`, comment)
-        res.json({comment: comment})
-    })
-    .catch(next)
-}
-)
+router.patch('/:commentId', requireToken, removeBlanks, (req, res, next) => {
+    console.log('Comment id: ', req.params.commentId)
+    console.log('req.usercomment: ', req.usercomment)
+    Comment.findById(req.params.commentId) // pass the id of target content
+        .then(handle404)
+        .then((comment) => {
+            console.log('this is the content: ', comment)
+            requireOwnership(req, comment) // check if user owns content
+            // returned updated value of content
+            return comment.updateOne(req.body.comment)
+        })
+        .then(() => 
+        res.sendStatus(204)) // successful 204 on updated 
+        .catch(next)
+  })
 
 //------------ Delete Comment ------------------
 // Delete ROUTES -> /comments/:userId
 
-router.delete(`/:userId`, (req,res, next)=>{
-    const userId = req.params.userId
-    Comment.findOneAndDelete({owner: userId})
-    .then(handle404)
-    .then(comment=> {
-        console.log(`DELETED A NEW COMMENT`, comment)
-        res.json({comment: comment})
-    })
-    .catch(next)
-}
-)
+router.delete('/delete/:id', requireToken, (req, res, next) => {
+    // Need userId to match the owner of the content
+    Comment.findById(req.params.id)
+        .then(handle404)
+        .then((comment) => {
+            // when content is passed, check if token matches that of loggedIn user token
+            // if matched, take content.id, and delete from the database
+            requireOwnership(req, comment)
+            comment.deleteOne()
+        })
+        .then(() => res.sendStatus(204)) // send a 204 <no content> on successful delete
+        .catch(next)
+  })
 
 module.exports = router 
